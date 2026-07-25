1f988becba8b8b5af3125c183b49c4c1921d0c62
Wizard-Gandalf <akmalfadli0002@gmail.com>
2026-07-26 00:52:44 +0700

    final true

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -76,8 +76,7 @@ function VideoSection() {
                 2026 di Desa Jaten.
               </p>
               <div className="flex gap-4" style={{ marginTop: 18 }}>
-                <span style={{ fontFamily: FONT_MONO, fontSize: 12, color: C.moringaLight }}>⏱ 5 MENIT</span>
-                <span style={{ fontFamily: FONT_MONO, fontSize: 12, color: C.moringaLight }}>📍 DESA JATEN</span>
+                <span style={{ fontFamily: FONT_MONO, fontSize: 12, color: C.moringaLight }}>3 MENIT</span>
               </div>
             </div>
           </div>
